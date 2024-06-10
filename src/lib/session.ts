9822c48d48d43ef6b43f18ae90be8a471edf438a
WZhijun154 <wangzhijun154@gmail.com>
2024-06-10 16:02:56 +0800

    comment out iron-session to avoid errors

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,17 +1,17 @@
 // lib/session.ts
-import { SessionOptions } from 'iron-session';
+// import { SessionOptions } from 'iron-session';
 
-export const sessionOptions: SessionOptions = {
-  password: 'wangzhijunasdajngmnifogbdvbdragsdaw3434r34fev',
-  cookieName: 'test_production',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24, // 1 day
-    sameSite: 'lax',
-    path: '/',
-  },
-};
+// export const sessionOptions: SessionOptions = {
+//   password: 'wangzhijunasdajngmnifogbdvbdragsdaw3434r34fev',
+//   cookieName: 'test_production',
+//   cookieOptions: {
+//     secure: process.env.NODE_ENV === 'production',
+//     maxAge: 60 * 60 * 24, // 1 day
+//     sameSite: 'lax',
+//     path: '/',
+//   },
+// };
 
-export interface IronSessionData {
-  id: string;
-}
+// export interface IronSessionData {
+//   id: string;
+// }
